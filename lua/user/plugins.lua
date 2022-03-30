058498f817fd5377638033ecd5bb7e0192102629
Constantin Ivanov <const@runecast.com>
2022-03-30 18:34:54 +0200

    my changes

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -60,8 +60,10 @@ return packer.startup(function(use)
   use "folke/which-key.nvim"
 
   -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
+  use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
   use "lunarvim/darkplus.nvim"
+  use "folke/tokyonight.nvim"
+  use "ishan9299/nvim-solarized-lua"
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
