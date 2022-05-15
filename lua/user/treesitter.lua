d24b48193f4be3862513bccf9d411a4522d1b768
Constantin Ivanov <const@runecast.com>
2022-05-15 11:45:17 +0200

    fixes after plugins update

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,9 +4,9 @@ if not status_ok then
 end
 
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-  ignore_install = { "" }, -- List of parsers to ignore installing
+  ignore_install = { "phpdoc" }, -- List of parsers to ignore installing
   autopairs = {
     enable = true,
   },
@@ -21,3 +21,6 @@ configs.setup {
     enable_autocmd = false,
   },
 }
+
+-- fix per:
+-- https://github.com/ogirginc/dotfiles/commit/3c85abd07227f9bab4398809668784e1aa03e1b1
---
+++ my_readme.txt
@@ -0,0 +1,11 @@
+fixes [2022-05-15]
+
+https://github.com/nvim-treesitter/nvim-treesitter/issues/2886
+fix in https://github.com/ogirginc/dotfiles/commit/3c85abd07227f9bab4398809668784e1aa03e1b1
+
+https://github.com/akinsho/bufferline.nvim/issues/375
+disable/enable akinsho/bufferline and akinsho/toggleterm
+
+https://github.com/hrsh7th/nvim-cmp/discussions/911
+
+
